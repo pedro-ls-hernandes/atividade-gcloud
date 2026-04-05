eb8656059ea45ec5ff21d2494ebda981a434121d
pedros.hernandes <pedro.s.hernandes845@gmail.com>
2026-04-05 14:39:36 -0300

    Commit inicial

---
+++ atividade-cloud/app/api/route.js
@@ -0,0 +1,7 @@
+export async function GET() {
+  return Response.json({
+    nome: "Pedro Lucas dos Santos Hernandes",
+    email: "pedro.s.hernandes845@gmail.com",
+    ra: "0201392321025"
+  });
+}
